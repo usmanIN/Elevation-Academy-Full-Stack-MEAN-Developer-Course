8c1e4bb963b66ba8948f44fcf10e69b46d5e765a
usmanIN <61617956+usmanIN@users.noreply.github.com>
2021-12-17 14:16:42 +0530

    update script

--- project/todo-app-js/script.js
+++ project/todo-app-js/script.js
@@ -1,69 +1,39 @@
 let database = [];
+var index = 1;
 document.getElementById("addList").addEventListener("click",function(){
     document.getElementById("model").style.display = "block";    
 });
 
-
 document.getElementById("addItem").addEventListener("click",function(){
-    let item = document.getElementById("Item").value;    
-    if(item!=''){
-        var object = Object({ list : item });
+    let title = document.getElementById("Item").value;    
+    if(title!=''){
+        const object = {id:index, topic : title, sublist: [] };
+        index+=1;
         database.push(object);
         document.getElementById("Item").value = "";
-        createElement(item);
+        createElement(object);
     }
     document.getElementById("model").style.display = "none";
 });
 
-
-
-document.getElementById("addListItem").addEventListener("click",function(){
-    let item = document.getElementById("listItem").value;    
-    if(item!=''){
-               
-        let p = document.createElement("p");
-        p.setAttribute("class","status");
-        p.innerText = "Task 1";
-        
-        let input = document.createElement("input");
-        input.setAttribute("type","checkbox");
-        p.appendChild(input);
-
-        
-
-        input.addEventListener("click",function(){
-            if(p.style.textDecoration == 'line-through'){
-                p.style.textDecoration = 'none';
-            }else{
-                p.style.textDecoration = 'line-through';
-            }
-        });
-
-
-    }
-    document.getElementById("listModel").style.display = "none";
-});
-
-
-function createElement(item){
-
+function createElement(object){
+    
     let card = document.createElement("div");
     card.setAttribute("class","card");      // build the div with .card classes
     let a = document.createElement("a");
     a.setAttribute("href","page.html?index"); // create element of anchor tags
     let h2 = document.createElement("h2");  // create element of h2 inside of a tags
-    h2.innerText = item;
+    h2.innerText = object.topic;
     let cardContent = document.createElement("div");  // create element of div tag inside parent div tags
     cardContent.setAttribute("class","card-content");
     cardContent.appendChild(document.createElement("hr")); 
-
 
     let cardButton = document.createElement("div");
     cardButton.setAttribute("class","card-button");
     let deleteButton = document.createElement("button");
     deleteButton.innerText = "Delete";
     let addButton = document.createElement("button");
-    addButton.setAttribute("id","addButton");
+    addButton.setAttribute("onclick","addSublist("+object.id+")");
     addButton.innerText = "Add";
 
     cardButton.appendChild(deleteButton);
@@ -75,19 +45,37 @@ function createElement(item){
     card.appendChild(cardButton);    
     document.getElementsByClassName("container")[0].appendChild(card);  
 
-
     deleteButton.addEventListener("click",function(){
         card.remove();
     });
-
-
-    addButton.addEventListener("click",function(){
-        document.getElementById("listModel").style.display = "block";             
-    });
-
-
-
-
 }
-   
 
+
+function addSublist(id){
+    let listModel = document.getElementById("listModel");
+    listModel.style.display = "block";     
+    document.getElementById("addListItem").addEventListener("click",function(){        
+        let item = document.getElementById("listItem").value;
+        if(item!=''){                         
+            database[id-1].sublist.push(item);
+            let p = document.createElement("p");
+            p.setAttribute("class","status");
+            p.innerText = item;            
+            let input = document.createElement("input");
+            input.setAttribute("type","checkbox");
+            p.appendChild(input);
+            //element.parentElement.parentElement.appendChild(p);
+            input.addEventListener("click",function(){
+                if(p.style.textDecoration == 'line-through'){
+                    p.style.textDecoration = 'none';
+                }else{
+                    p.style.textDecoration = 'line-through';
+                }
+            });
+            console.log(database);
+        }
+    document.getElementById("listItem").value='';
+    listModel.style.display = "none";
+    
+    });
+}
