fe0faf4c12ece45c2ed31432e5ab2dbd6bd42eaf
usmanIN <61617956+usmanIN@users.noreply.github.com>
2021-12-27 01:23:31 +0530

    added routers

--- react/assignment/assignment_1/src/component/Parent.js
+++ react/assignment/assignment_1/src/component/Parent.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'
+import {BrowserRouter,Routes,Route,Navigate} from "react-router-dom";
 import Header from './assignment_1/Header'
+import Page from './assignment_2/Page';
+import PageNotFound from './PageNotFound';
 
 class Parent extends Component{
 
     render(){
         return(
-            <Header />
-        )
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Navigate to='/assignment_1'/>} />
+                    <Route path='/assignment_1' element={<Header />} />
+                    <Route path='/assignment_2' element={<Page />} />                    
+                    <Route path='/*' element={<PageNotFound />} />                    
+                    {/* <Route
+      path="*"
+      element={
+        <main style={{ padding: "1rem" }}>
+          <h2>There's nothing here!</h2>
+        </main>
+      }
+    /> */}
+                </Routes>            
+            </BrowserRouter>
+
+        
+        );
     }
 }
 
