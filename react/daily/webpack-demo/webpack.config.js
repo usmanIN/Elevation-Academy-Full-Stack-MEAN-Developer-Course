9716df5c5212c7835107682f958f00f68117287e
usmanIN <mohdusmangani95@gmail.com>
2022-03-01 21:17:22 +0530

    update the webpack demo

--- react/daily/webpack-demo/webpack.config.js
+++ react/daily/webpack-demo/webpack.config.js
@@ -1,16 +1,32 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: './app/index.js', // It required entry point for your application
+    entry: './src/index.js',
     module: {
         rules: [
-          {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-          },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader'
+            },
+            {
+                test: /\.(js)$/,
+                use: 'babel-loader'
+            }
+
         ],
-      },
-    output:{
-        path: path.resolve(__dirname,'dist'),  // It require absolute paths i.e. c:/drive/folder/sub-folder ...
-        filename: 'bundle.js', // filename of your output file
-    }
+    },
+    output: {
+        path : path.resolve(__dirname,"dist"),
+        filename: "bundle.js"
+    },
+    plugins: [new HtmlWebpackPlugin()],
+    mode: 'production'
+
 }
+
+// pr
