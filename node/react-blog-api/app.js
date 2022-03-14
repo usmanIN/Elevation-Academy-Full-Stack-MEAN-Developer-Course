2fc06fe254f7a027f8665a5a6782066fe8f68686
usmanIN <mohdusmangani95@gmail.com>
2022-03-14 14:47:36 +0530

    added the cors libraries in node api

--- node/react-blog-api/app.js
+++ node/react-blog-api/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.port || 8000;
-
 const path = require("path");
+const cors = require("cors");
 
+app.use(cors());
 app.set("views",path.resolve(__dirname,'./views'))
 app.set('view engine','pug')
 
