36d6c24871646ca7964ef1d101795ab6b11092fe
Mohd Usman Gani <61617956+usmanIN@users.noreply.github.com>
2022-04-12 20:46:33 +0530

    added the package.json for react api

--- node/react-blog-api/app.js
+++ node/react-blog-api/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 
 const path = require("path");
 const cors = require("cors");
